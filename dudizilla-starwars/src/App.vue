bcbb93eccca3a4bc5d07ef69d317e570e6c15b35
dudizilla <eduarda.mcunha@outlook.com>
2021-10-26 00:02:51 -0300

    theme toggle

--- dudizilla-starwars/src/App.vue
+++ dudizilla-starwars/src/App.vue
@@ -1,9 +1,15 @@
 <template>
+    <theme-button/>
     <router-view />
 </template>
 
 <script lang="ts">
+import ThemeButton from './components/forms/ThemeButton.vue'
+
 export default {
+    components: {
+        ThemeButton
+    }
 }
 </script>
 
